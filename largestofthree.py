e7ed260e83a4422143194141745744531983a998
Salman Nazeer <salmannazeer477@gmail.com>
2026-07-29 10:48:16 +0530

    changed formatting

--- largestofthree.py
+++ largestofthree.py
@@ -1,7 +1,5 @@
 a = int(input('enter first number:'))
 b = int(input('enter second number:'))
 c = int(input('enter third number:'))
-
 largest = a if a > b and a > c else b if b > c else c
-
 print(f"largest number is {largest}")
--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,2 @@
-
 text = input("Enter a text:")
-
 print("palindrome") if text == text[::-1] else print("not a palindrome")
